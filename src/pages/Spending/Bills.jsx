71788ec8a8afa0babeb2bbbcf89e554f97b4c956
me <coderwj@live.com>
2025-02-16 16:28:12 +0800

    优化展示

--- src/pages/Spending/Bills.jsx
+++ src/pages/Spending/Bills.jsx
@@ -1,19 +1,40 @@
 import {TablePageable} from "../../compose/Table";
 import {API_COLLECTOR} from "../../config/route";
 import {LongText, textTimeTrim} from "../../compose/Compose";
+import {toSelectOptions} from "../../compose/Form";
+import {platformMapping, stateMapping} from "../../util/value.mapping";
+import {Typography} from "antd";
 
 export const Bills = () => {
-    return <TablePageable url={`${API_COLLECTOR}/spending/payInfo/page`} columns={columns}/>
+    return <TablePageable url={`${API_COLLECTOR}/spending/payInfo/page`}
+                          columns={columns} query={query}/>
 }
 
 const columns = [
     {title: '编号', dataIndex: 'id',},
     {title: '平台', dataIndex: 'platform',},
     {title: '地点', dataIndex: 'place',},
-    {title: '金额', dataIndex: 'money',},
-    {title: '余额', dataIndex: 'balance',},
+    {
+        title: '金额', dataIndex: 'money', render: (v, it) => <Typography.Text
+            type={it.revenue ? 'success' : 'warning'}>{`${it.revenue ? '+' : '-'}${v}`}</Typography.Text>
+    },
+    {
+        title: '余额', dataIndex: 'balance', render: (v) => <Typography.Text
+            type={Number(v) < 800 ? 'secondary' : 'default'}>{v}</Typography.Text>
+    },
     {title: '备注', dataIndex: 'remark',},
     {title: '原始信息', dataIndex: 'information', render: v => <LongText text={v}/>},
     {title: '信息来源', dataIndex: 'source',},
     {title: '什么时候', dataIndex: 'time', render: textTimeTrim},
+]
+
+const query = [
+    {title: '平台', dataIndex: 'platform', type: 'select', options: toSelectOptions(platformMapping)},
+    {title: '地点', dataIndex: 'place',},
+    {title: '备注', dataIndex: 'remark',},
+    {title: '原始信息', dataIndex: 'information',},
+    {title: '收入', dataIndex: 'revenue', type: 'boolean'},
+    {title: '忽略统计', dataIndex: 'ignoreStatistics', type: 'boolean'},
+    {title: '什么时候', dataIndex: 'time', type: 'timeRange'},
+    {title: '倒序', dataIndex: 'reverseOrder', type: 'switch', defaultValue: true},
 ]
--- src/util/value.mapping.js
+++ src/util/value.mapping.js
@@ -1,6 +1,6 @@
 import {ifNull} from "./es.util";
 
-const listToObj = (list, valueMapping) => {
+const listToObj = (list, valueMapping = it => it.name) => {
     const obj = {};
     list.forEach(it => obj[it.key] = valueMapping(it));
     return obj;
@@ -13,6 +13,27 @@ export const stateList = [
     {key: 4, name: '错误'},
 ]
 
-export const stateMapping = listToObj(stateList, it => it.name);
+export const stateMapping = listToObj(stateList);
 
 export const stateText = v => ifNull(stateMapping[v], v);
+
+export const platformList = [
+    {key: "支付宝", name: "支付宝", icon: null},
+    {key: "微信", name: "微信", icon: null},
+    {key: "交通银行", name: "交通银行", icon: null},
+    {key: "ETC", name: "ETC", icon: null},
+    {key: "银联", name: "银联", icon: null},
+    {key: "招商银行", name: "招商银行", icon: null},
+    {key: "云闪付", name: "云闪付", icon: null},
+    {key: "网上国网", name: "网上国网", icon: null},
+    {key: "三星Pay", name: "三星Pay", icon: null},
+    {key: "京东", name: "京东", icon: null},
+    {key: "中国铁路", name: "中国铁路", icon: null},
+    {key: "小红书", name: "小红书", icon: null},
+    {key: "拼多多", name: "拼多多", icon: null},
+    {key: "美团", name: "美团", icon: null},
+    {key: "抖音", name: "抖音", icon: null},
+    {key: "华为商城", name: "华为商城", icon: null},
+]
+
+export const platformMapping = listToObj(platformList)

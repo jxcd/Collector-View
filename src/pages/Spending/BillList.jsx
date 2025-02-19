20f23671c8bfc1ae9abe6ff833fc6c67994a6ebd
jxcd <53029929+jxcd@users.noreply.github.com>
2025-02-19 23:14:17 +0800

    新增统计, 修改详情为Popover点击触发

--- src/pages/Spending/BillList.jsx
+++ src/pages/Spending/BillList.jsx
@@ -2,16 +2,15 @@ import {TablePageable} from "../../compose/Table";
 import {API_COLLECTOR} from "../../config/route";
 import {LongText, textTimeTrim} from "../../compose/Compose";
 import {toSelectOptions} from "../../compose/Form";
-import {platformIconMapping, platformMapping, stateMapping} from "../../util/value.mapping";
-import {Col, Collapse, Flex, Row, Typography} from "antd";
-import {Avatar, List, message} from 'antd';
+import {platformIconMapping, platformMapping} from "../../util/value.mapping";
+import {Card, Collapse, Flex, List, Popover, Statistic, Typography} from "antd";
 import VirtualList from 'rc-virtual-list';
 import React, {useEffect, useRef, useState} from "react";
-import {isBlack, notBlack, postWithHandler} from "../../util/es.util";
-import {BugOutlined} from "@ant-design/icons";
+import {notBlack, postWithHandler} from "../../util/es.util";
+import {PayCircleFilled} from "@ant-design/icons";
 
 export const BillList = () => {
-    const ContainerHeight = window.innerHeight ;
+    const ContainerHeight = window.innerHeight;
     const current = 1;
     const pageSize = 10;
 
@@ -24,6 +23,7 @@ export const BillList = () => {
         data: [],
     });
     const idSet = useRef(new Set());
+    const [statistics, setStatistics] = useState([]);
 
     const appendData = (current, pageSize = 20) => {
         postWithHandler(`${API_COLLECTOR}/spending/payInfo/page`, {where, current, pageSize}, it => {
@@ -41,7 +41,8 @@ export const BillList = () => {
     }
 
     useEffect(() => {
-        appendData(1)
+        postWithHandler(`${API_COLLECTOR}/spending/payInfo/cycleStatistics`, [1, 7, 30], setStatistics)
+        appendData(1);
     }, []);
 
     const onScroll = e => {
@@ -50,127 +51,73 @@ export const BillList = () => {
             sp: e.currentTarget.scrollTop,
             ch: ContainerHeight,
         })
-        if (Math.abs(e.currentTarget.scrollHeight - e.currentTarget.scrollTop - ContainerHeight) <= 1) {
+        if (Math.abs(e.currentTarget.scrollHeight - e.currentTarget.scrollTop - ContainerHeight) <= 100) {
             appendData(page.current + 1)
         }
     }
 
     return <List>
-        <VirtualList data={page.data} height={ContainerHeight} itemHeight={47} itemKey={"id"} onScroll={onScroll}>
-            {(it) => {
-                const itemsCollapse = [
-                    {
-                        key: '1', label: it.place, children: <Flex vertical={true} gap={"small"}>
-                            <Typography.Text>{it.information}</Typography.Text>
-                            {notBlack(it.remark) && <Typography.Text>{it.remark}</Typography.Text>}
-                        </Flex>
-                    }
-                ];
+        <List.Item>
+            <Flex gap={"middle"} wrap={"wrap"} justify={"space-around"} style={{width: '100%'}}>
+                <>{statistics.map(it =>
+                    <Card key={it.cycle} title={`${it.cycle}天内`} style={{width: '800'}} hoverable={true}>
+                        <Statistic
+                            title={`收入 ${it.incomeCount}笔`}
+                            value={it.income}
+                            valueStyle={{color: '#52c41a'}}
+                            suffix={<PayCircleFilled/>}
+                        />
+                        <Statistic
+                            title={`消费 ${it.consumptionCount}笔`}
+                            value={it.consumption}
+                            valueStyle={{color: '#faad14'}}
+                            suffix={<PayCircleFilled/>}
+                        />
 
-                const Icon = platformIconMapping[it.platform];
+                    </Card>)}</>
+            </Flex>
+        </List.Item>
 
-                return (
-                    <List.Item key={it.id}>
-                        <Flex gap={"large"} align={"center"} justify={"space-evenly"} style={{width: '100%'}}
-                              wrap={"wrap"}>
-                            {React.cloneElement(Icon, {style: {fontSize: 48}})}
-                            <Typography.Text style={{fontSize: 36, width: 160}}
-                                // style={{width: '80'}}
-                                             type={it.revenue ? 'success' : 'warning'}>{`${it.revenue ? '+' : '-'}${it.money}`}
-                            </Typography.Text>
-
-                            <Flex vertical={true}>
-                                <Typography.Text>{textTimeTrim(it.time)}</Typography.Text>
-                                <Typography.Text>{`余额: ${it.balance}`}</Typography.Text>
-                            </Flex>
-
-                            <Collapse  style={{minWidth: '50vw'}} items={itemsCollapse} />
-                        </Flex>
-                        {/*<Row style={{width: '100%'}} gutter={{*/
-                        }
-                        {/*    xs: 8,*/
-                        }
-                        {/*    sm: 16,*/
-                        }
-                        {/*    md: 24,*/
-                        }
-                        {/*    lg: 32,*/
-                        }
-                        {/*}}>*/
-                        }
-                        {/*    /!*<Flex gap={"middle"}>*!/*/
-                        }
-                        {/*    <Col span={2}>*/
-                        }
-                        {/*        <BugOutlined/>*/
-                        }
-                        {/*    </Col>*/
-                        }
-                        {/*    <Col span={2}>*/
-                        }
-                        {/*        <Typography.Text*/
-                        }
-                        {/*            type={it.revenue ? 'success' : 'warning'}>{`${it.revenue ? '+' : '-'}${it.money}`}*/
-                        }
-                        {/*        </Typography.Text>*/
-                        }
-                        {/*    </Col>*/
-                        }
-
-                        {/*    <Col span={16}>*/
-                        }
-                        {/*        <Flex vertical={true}>*/
-                        }
-                        {/*            <Typography.Text>{textTimeTrim(it.time)}</Typography.Text>*/
-                        }
-
-                        {/*            <Collapse items={itemsCollapse}/>*/
-                        }
-                        {/*        </Flex>*/
-                        }
-                        {/*    </Col>*/
-                        }
-
-                        {/*    <Col span={2}>*/
-                        }
-                        {/*        <Typography.Text>{it.balance}</Typography.Text>*/
-                        }
-                        {/*    </Col>*/
-                        }
-                        {/*    /!*</Flex>*!/*/
-                        }
-                        {/*</Row>*/
-                        }
-
-                        {/*<List.Item.Meta*/
-                        }
-                        {/*    avatar={<BugOutlined/>}*/
-                        }
-                        {/*    title={<Flex gap={"large"} wrap={"wrap"}>*/
-                        }
-                        {/*        {textTimeTrim(it.time)}*/
-                        }
-
-                        {/*        <Typography.Text*/
-                        }
-                        {/*            type={it.revenue ? 'success' : 'warning'}>{`${it.revenue ? '+' : '-'}${it.money}`}*/
-                        }
-                        {/*        </Typography.Text>*/
-                        }
-                        {/*    </Flex>}*/
-                        }
-                        {/*    description={<Collapse items={itemsCollapse}/>}*/
-                        }
-                        {/*/>*/
-                        }
-                    </List.Item>
-                )
-                    ;
-            }}
+        <VirtualList data={page.data} height={ContainerHeight} itemHeight={48} itemKey={"id"} onScroll={onScroll}>
+            {rowItem}
         </VirtualList>
     </List>
 
 }
+
+const rowItem = (it) => {
+    const Icon = platformIconMapping[it.platform];
+
+    return (
+        <List.Item key={it.id}>
+            <Popover
+                trigger={"click"}
+                content={<Flex vertical={true} gap={"small"}>
+                    <Typography.Text>{it.information}</Typography.Text>
+                    {notBlack(it.remark) && <Typography.Text>{it.remark}</Typography.Text>}
+                </Flex>}
+            >
+                <Flex justify={"space-between"} wrap={"wrap"}
+                      style={{width: '100%', paddingLeft: 16, paddingRight: 16}}>
+                    <Flex gap={"large"} align={"center"} justify={"space-evenly"} wrap={"wrap"}>
+                        {React.cloneElement(Icon, {style: {fontSize: 48}})}
+                        <Typography.Text style={{fontSize: 36, width: 160}}
+                            // style={{width: '80'}}
+                                         type={it.revenue ? 'success' : 'warning'}>{`${it.revenue ? '+' : '-'}${it.money}`}
+                        </Typography.Text>
+
+                        <Flex vertical={true}>
+                            <Typography.Text>{textTimeTrim(it.time)}</Typography.Text>
+                            <Typography.Text>{`余额: ${it.balance}`}</Typography.Text>
+                        </Flex>
+
+                    </Flex>
+                    <Typography.Text style={{fontSize: 24}}>{it.place}</Typography.Text>
+                </Flex>
+            </Popover>
+        </List.Item>
+    )
+};
 
 export const Bills2 = () => {
     return <TablePageable url={`${API_COLLECTOR}/spending/payInfo/page`}
